9e8684eab229bd94d62ea6bdbd6da991e231fca3
Alexandra Kalisz <alexkalisz@gmail.com>
2024-06-10 17:12:09 +0000

    set up structure for game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log ("Connected")
+function runGame() {}
+
+
+function checkAnswer {}
+
+function calculateComputerChoice(){}
+
+function incrementScore(){}
+
+function reduceTries() {}
+
+function resetGame() {}
+
+function handleButtonClick(){}
+
+/* code source: https://stackoverflow.com/questions/76196162/function-that-gets-button-click */
+
+function attachButtonEventListeners(){}
+
+/* code source : https://stackoverflow.com/questions/57742395/how-do-i-make-a-function-that-reset-the-game-on-click */
+
+function initializeGame() {}
+
