7a096eb400e8a9ed2f9a2e21d4028728beaeb883
Alexandra Kalisz <alexkalisz@gmail.com>
2024-06-10 19:20:58 +0000

    add event listeners for document loading and button clicks plus allerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let dataType = this.getAttribute("data-type");
+            if (dataType === "reset") {
+                alert("Reset button clicked. The game will be reset.");
+                resetGame();
+            } else {
+                let playerChoice = dataType;
+                alert(`Button clicked: ${playerChoice}. Starting game.`);
+                runGame(playerChoice);
+            }
+        });
+    }
+
+
+    runGame();
+});
+
 function runGame() {}
 
 
